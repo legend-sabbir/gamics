fa49b5210d01c5e2ab315d63630137d853b4d94d
Legend <sabbirhossain.trash@gmail.com>
2022-09-11 17:23:01 +0600

    initial

--- scss.sh
+++ scss.sh
@@ -1,5 +1,5 @@
 #! /data/data/com.termux/files/usr/bin/bash 
 
 while inotifywait index.html; do
-  sass scss/style.scss --watch --no-source-map css/style.css 
+  sass scss/style.scss --watch --no-source-map css/style.min.css --style compressed 
 done
